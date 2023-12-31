7c1e40f44cdf7892c33a23b2d744dc9b824f7f20
Aditya-Singh536 <129666207+Aditya-Singh536@users.noreply.github.com>
2023-12-31 02:01:25 -0500

    Add files via upload
    It calculates speed or time or distance as per users choice.

---
+++ SpeedTimeDistancefinder.py
@@ -0,0 +1,22 @@
+def STDfinder():
+    a = int(input('Enter the distance in km:'))
+    b = int(input('Enter the time in h:'))
+    c = int(input('Enter the speed in km/h:'))
+    s = 's'
+    t = 't'
+    d = 'd'
+
+    print('What to find?\n')
+    
+    y = input('If distance:d,time:t,speed:s:')
+
+    if y == s:
+        print(a/b,'km/h is your speed.')
+    elif y == t:
+        print(a/c,'h is your time.')
+    elif y == d:
+        print(b*c,'km is your distance.')
+
+print('If don\'t known value give it "0"\n')
+
+STDfinder()
